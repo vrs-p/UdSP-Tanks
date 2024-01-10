8cb3e30b1cd2914048addcd8dd3276871d8ebedd
vrsp <verespatrik3@gmail.com>
2024-01-10 14:50:50 +0100

    added locale on server side

--- server-side/main.c
+++ server-side/main.c
@@ -1,6 +1,9 @@
+#include <locale.h>
 #include "server_controller/server_controller.h"
 
 int main() {
+    setlocale(LC_ALL, "sk_SK.UTF-8");
+
     SERVER_CONTROLLER controller;
     controller_create(&controller);
     controller_start(&controller);
